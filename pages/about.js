528a55c10eee310dad214f6b90a0ad84ea117f63
ffutop <17816873960@163.com>
2019-05-04 18:42:50 +0800

    init repo

---
+++ pages/about.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import '../static/styles.scss';
+
+const About = props => {
+  return (
+    <div className="page-about">
+      <div className="about-style">
+        <div className="about-head">
+          <img className="about-img" src='/static/images/AboutMe.jpeg' alt="avator" />
+          <div className="about-intro">
+            <h1 className="about-name">防风</h1>
+            <ul className="about-tag">
+              <li>前算法爱好者</li>
+              <li>Java程序猿</li>
+              <li>Kernel探索者</li>
+            </ul>
+            <div className="about-skill">
+              <h2>Programming Skill</h2>
+              <ul>
+                <li>C</li>
+                <li>Java</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-desc" />
+        </div>
+        <div className="about-contact">
+          <figure className="about-icon">
+            <img src='/static/images/icon.png' alt="icon" />
+          </figure>
+          <ul>
+            <li>
+              <i className="fas fa-link" /> <a href="/">https://www.ffutop.com/</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <i className="fas fa-at" />{' '}
+              <a href="mailto:dormousenone@gmail.com">dormousenone@gmail.com</a>
+            </li>
+            <li>
+              <i className="fas fa-map-marked-alt" /> Hangzhou, China
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <p>ffutop's blog</p>
+    <Link href="/about">
+      About Me
+    </Link>
+  </div>
+)
+
+export default Index;
